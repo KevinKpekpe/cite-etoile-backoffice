4e620659e10d3487d0e7c1f9f5c265e7ad84288b
Spectre Coding <kpekpekevin57@gmail.com>
2026-09-25 13:34:20 +0100

    fix(config): trust proxy headers for Cloudflare Tunnel HTTPS asset generation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             '2fa' => EnsureTwoFactorChallengeIsComplete::class,
             'force_password_change' => EnsurePasswordIsChanged::class,
