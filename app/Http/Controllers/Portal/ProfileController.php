bb5039b521293f112e162e004fc9999bbbca9db5
Spectre Coding <kpekpekevin57@gmail.com>
2026-09-25 08:02:28 +0100

    fix(controllers): add missing DB and Hash facade imports in profile controllers

--- app/Http/Controllers/Portal/ProfileController.php
+++ app/Http/Controllers/Portal/ProfileController.php
@@ -10,6 +10,8 @@ use App\Models\Customer;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
